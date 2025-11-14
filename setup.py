acbefc371e8ff05cb4a48dc755deaca770a692bb
ljaquier <83839893+ljaquier@users.noreply.github.com>
2025-11-14 12:23:09 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gpx2strava',
+    version='1.0.0',
+    url='https://github.com/ljaquier/ctb2strava',
+    author='ljaquier',
+    author_email='83839893+ljaquier@users.noreply.github.com',
+    description='Simple Python script/library that allows to create an activity on Strava from a GPX file.',
+    packages=find_packages(),    
+    install_requires=['gpxpy', 'requests']
+)
